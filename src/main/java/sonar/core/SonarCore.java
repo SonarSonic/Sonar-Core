bb9e38e68e3eda99a74123b388c5bd90e0e4e6d7
SonarSonic <ollielansdell@hotmail.co.uk>
2017-09-19 17:16:35 +0100

    Fixes Version Range

--- src/main/java/sonar/core/SonarCore.java
+++ src/main/java/sonar/core/SonarCore.java
@@ -58,7 +58,7 @@ public class SonarCore {
 	public static final String name = "SonarCore";
 	public static final String modid = "sonarcore";
 	public static final String version = "5.0.1";
-	public static final String mc_versions = "[1.12,1.12.1,1.12.2]";
+	public static final String mc_versions = "[1.12,1.12.2]";
 
 	@SidedProxy(clientSide = "sonar.core.network.SonarClient", serverSide = "sonar.core.network.SonarCommon")
 	public static SonarCommon proxy;
